112acc2ed35466f6aa03310cfa85d1f087212cbc
Adrian554-dev <a24adrianbr@iessanclemente.net>
2025-05-20 13:43:08 +0200

    entry

--- apache/entrypoint.sh
+++ apache/entrypoint.sh
@@ -5,6 +5,7 @@ rm -rf /app/var/cache/*
 mkdir -p /app/var/cache/
 mkdir -p /app/var/logs/
 mkdir -p /app/cache/
+mkdir -p /app/themes/classic/assets/cache/
 
 chmod a+w -R /app/var/logs
 chmod a+w -R /app/var/cache
