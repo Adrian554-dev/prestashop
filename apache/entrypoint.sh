8f7b1c6a31fcd423826b14fcb0f50096a60dd885
Adrian554-dev <a24adrianbr@iessanclemente.net>
2025-05-20 13:34:49 +0200

    entrypoint

--- apache/entrypoint.sh
+++ apache/entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+
+rm -rf /app/var/cache/*
+mkdir -p /app/var/cache/
+mkdir -p /app/var/logs/
+mkdir -p /app/cache/
+
 chmod a+w -R /app/var/logs
 chmod a+w -R /app/var/cache
-rm -rf /app/var/cache/*
-
-
-mkdir /kk
+chmod a+w -R /app/cache
 
 
 apachectl -D FOREGROUND
