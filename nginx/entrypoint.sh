44011e2ae789322f2471423a8f777f7e2f9feacd
adrian@debianDAW <a24adrianbr@iessanclemente.net>
2025-05-26 21:03:15 +0200

    nginx

---
+++ nginx/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+rm -rf /app/var/cache/*
+mkdir -p /app/var/cache/
+mkdir -p /app/var/logs/
+mkdir -p /app/cache/
+mkdir -p /app/themes/classic/assets/cache/
+
+chmod a+w -R /app/var/logs
+chmod a+w -R /app/var/cache
+chmod a+w -R /app/cache
+
+chmod a+w -R /app/themes/classic/assets/cache/
+
+
+apachectl -D FOREGROUND
+#tail -f /dev/null
